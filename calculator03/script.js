46bdf19d72e8696ed2c1623afe6b0d0a5376c103
hansoloh <seoyeonoh1022@gmail.com>
2021-05-25 19:07:33 +0900

    cal restart

--- calculator03/script.js
+++ calculator03/script.js
@@ -3,34 +3,8 @@ let buttons = Array.from(document.getElementsByClassName('button'));
 let count = 0;
 let number = [];
 
+function reset(){}
 
-buttons.map(button => {
-    button.addEventListener('click', (e) => {        
+function calculate(){}
 
-        switch (e.target.innerText) {
-            case 'C':
-                display.value="";
-                break;
-            case '=':
-                display.innerText = Math.floor(eval(display.innerText));
-                break;
-            default:
-                display.innerText += e.target.innerText;
-                console.log(display.innerText);
-
-                if(count < 3){
-                    number.push(display.innerText);
-                    count++;
-                }else{
-                    console.log("4자리 누르면 ...버튼 눌러도 입력이 안되게");  
-                }       
-                console.log(number);                
-                console.log(count);         
-        }
-
-    });
-});
-
-
-// 부호 중복 입력 가능 오류
-// 최대 입력 가능 숫자 3자리
+function button(){}
