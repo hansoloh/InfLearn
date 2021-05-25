46871532669598f3e6d24aed32a989ac250cda2c
hansoloh <seoyeonoh1022@gmail.com>
2021-05-26 00:10:26 +0900

    calc

--- calculator03/script.js
+++ calculator03/script.js
@@ -1,10 +1,30 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
-let count = 0;
-let number = [];
+let click = true;
 
-function reset(){}
+function number(num) {
+    let display = document.getElementById("display");
+    display.value = display.value + num;
+}
 
-function calculate(){}
+function sign(sn) {
+    if (click) {
+        let display = document.getElementById("display");
+        display.value = display.value + sn;
+        click = false;
+    } else {
+        console.log("부호 입력 끝");
+    }
+}
 
-function button(){}
+function calculate() {
+    let display = document.getElementById("display");
+    let result = eval(display.value);
+    let displayResult = document.getElementById("result");
+    displayResult.value = (Math.floor(result));
+}
+
+function reset() {
+    let display = document.getElementById("display");
+    display.value = "";
+    let displayResult = document.getElementById("result");
+    displayResult.value = "";
+}
