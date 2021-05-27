ca78bbfb018fd802c6fa38b466264e82c9214b9d
hansoloh <seoyeonoh1022@gmail.com>
2021-05-27 17:21:55 +0900

    cal FINAL

--- calculator03/script.js
+++ calculator03/script.js
@@ -1,14 +1,33 @@
 let check = 0; //숫자 함수 입력 자릿수 카운팅 위한 변수
 //let click = true; //부호 함수 체크 위한 boolean
 const result = []; //결과값 담아줄 배열
+let display = document.getElementById("display");
+let displayResult = document.getElementById("result");
 
 function number(num) { //숫자 함수
-    if (check <= 2) {
+
+    if (result.length === 0 && num === 0) { //맨 처음에 0을 누르면 false. 전체 함수 실행 안됨.
+        return false;
+    }
+    console.log(typeof result[result.length - 1]); //그 담부터 값 입력할때 값(타입)을 판단
+    if (typeof result[result.length - 1] === 'string' && num === 0) { // 부호거나 0일때 false return. 입력 안되게.
+        return false;
+    }
+
+    if (check <= 2) { //배열 길이 4까지만 생성됨
+        // if (check==0 && num ==0){
+        //     break;
+        //    }
+
+        // if(result[0] == 0){ //맨 처음에 0 입력되지 않게
+        //     result.length = 0;
+        //     display.value = '';
+        //  } 
         check++;
-        let display = document.getElementById("display");
         display.value = display.value + num;
-        result.push(num);
+        result.push(num); 
         //click = true; //여기에 click값 true 초기화해도 된다고? 왜?
+        //console.log(result[0]);                   
     }
     console.log(result);
     //console.log(check);
@@ -23,21 +42,23 @@ function number(num) { //숫자 함수
 function sign(sn) { //부호 함수   
     //if (click = true) {
         if (-1 < result[result.length - 1] && result[result.length - 1] < 10) { //마지막 인덱스 값이 숫자면 (0~9까지 숫자를 눌렀을때)
-            let display = document.getElementById("display");
             display.value = display.value + sn;
             result.push(sn);
-            console.log(result[result.length - 1]); 
+            //console.log(result[result.length - 1]); 
         } else { //마지막 인덱스 값이 부호면 
-            let display = document.getElementById("display");
-            //console.log('마지막 인덱스를 지금 입력한 부호로 교체');
-            result[result.length - 1] = sn; //마지막 인덱스를 지금 입력한 부호sn로 교체
+            result[result.length - 1] = sn; //마지막 인덱스를 지금 입력한 부호sn로 교체            
             //display.value[display.value.length - 1] = sn;
-            console.log(display.value); 
+            //console.log(sn) 
+            //console.log(result.indexOf(sn));                         
+            //console.log(result.length); 
+            //console.log(result.indexOf(sn)+1); //부호 다음 인덱스 ok
+            //console.log(result[result.indexOf(sn)+1]); //부호 다음 인덱스의 값 받아오기...            
+
             //배열에는 부호 바뀜! ! display에 안바뀜 
             //display.value = result; //결과값이 배열로 나옴..., , ,지우기...
-            display.value = result.join(""); 
+            display.value = result.join("");               
         }
-        check = 0;
+        check = 0;               
     }
 //}
 
@@ -46,23 +67,24 @@ function sign(sn) { //부호 함수
 //숫자 3자리, 부호 1개, 숫자 3자리까지 누르면 그대로 끝 반복 진행 안됨. -> 또 리셋해주기? 어디서?
 
 function calculate() {
-    let display = document.getElementById("display");
     let result = eval(display.value);
     let displayResult = document.getElementById("result");
     displayResult.value = (Math.floor(result));
 }
 
 function reset() {
-    let display = document.getElementById("display");
     display.value = "";
-    let displayResult = document.getElementById("result");
     displayResult.value = "";
 
     check = 0;
 
     //c 리셋하면 display에는 지워지는데 배열은 이어서 담아짐. 배열 리셋 
     //result=[];로 리셋안됨....부호 눌러서 배열에 문자 들어가서인...가...?
+    console.log(result);
     result.length = 0; //.....되다니
+    console.log(result);
+
+}
 
     //click = true;
     //숫자 부호 숫자 결과 c누르면 리셋까지 됨. check랑click=0이라서 다시 계산 시작 가능.
@@ -81,4 +103,8 @@ function reset() {
     //onclick 발생할때마다 boolean값 리셋되기?
     //onclick 1회 -> sign값 입력받음 -> 다음 onclick 2회 -> sign값 리셋되고 다시 새값 입력받기
     //현재 sign onclick 클릭하면 click=false되어있는 상태. 이걸 다시 true로 리셋? 어디서?
-}
+
+
+// display.value[display.value.length - 1] == result[result.length - 1]
+// result[result.length - 1]필요없음? 배열 필요 없음?
+// display.value.length 써서 더 간단하게 수정해보기
