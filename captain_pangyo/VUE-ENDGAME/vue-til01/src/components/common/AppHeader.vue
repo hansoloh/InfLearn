6e756a39f8032d67960f4810f81e4e587aec4fe6
hansoloh <seoyeonoh1022@gmail.com>
2021-07-14 19:07:12 +0900

    0714

---
+++ captain_pangyo/VUE-ENDGAME/vue-til01/src/components/common/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+//import Demo from '../../demo/basic/Demo.vue'
+import Demo from '@/demo/basic/Demo';
+
+export default {};
+</script>
+
+<style></style>
