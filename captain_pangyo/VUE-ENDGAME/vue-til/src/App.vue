b66ab88f52ccb32e4898866f3b91fd7b0e417201
hansoloh <seoyeonoh1022@gmail.com>
2021-07-27 15:20:39 +0900

    0727

--- captain_pangyo/VUE-ENDGAME/vue-til/src/App.vue
+++ captain_pangyo/VUE-ENDGAME/vue-til/src/App.vue
@@ -1,35 +1,15 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-	</div>
+  <div>
+    <header>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">Signup</router-link>
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import AppHeader from './components/common/AppHeader.vue';
-
-var a = 'hi';
-
-export default {
-	name: 'app',
-	components: {
-		HelloWorld,
-		AppHeader,
-	},
-	created() {
-		console.log('hi');
-	},
-};
+export default {};
 </script>
 
-<style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
+<style></style>
