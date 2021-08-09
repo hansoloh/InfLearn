dacb1717e2da288d653f104c0646f45a435b964b
hansoloh <seoyeonoh1022@gmail.com>
2021-08-09 19:00:13 +0900

    0809

--- captain_pangyo/VUE-ENDGAME/vue-til/src/components/posts/PostListItem.vue
+++ captain_pangyo/VUE-ENDGAME/vue-til/src/components/posts/PostListItem.vue
@@ -7,8 +7,8 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
-      {{ postItem.createdAt }}
-      <i class="icon ion-md-create"></i>
+      {{ postItem.createdAt | formatDate }}
+      <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
@@ -26,12 +26,17 @@ export default {
   },
   methods: {
     async deleteItem() {
-      if (confirm('delete?')) {
+      if (confirm('You want to delete it?')) {
         await deletePost(this.postItem._id);
         this.$emit('refresh');
       }
     },
+    routeEditPage() {
+      const id = this.postItem._id;
+      this.$router.push(`/post/${id}`);
+    },
   },
 };
 </script>
+
 <style></style>
