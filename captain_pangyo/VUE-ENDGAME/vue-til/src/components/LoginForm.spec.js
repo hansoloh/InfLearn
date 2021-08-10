8f0f0a161782dfcf5e614624deef934a6ae55c4c
hansoloh <seoyeonoh1022@gmail.com>
2021-08-10 12:14:14 +0900

    0810 done

--- captain_pangyo/VUE-ENDGAME/vue-til/src/components/LoginForm.spec.js
+++ captain_pangyo/VUE-ENDGAME/vue-til/src/components/LoginForm.spec.js
@@ -1,9 +1,32 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const instance = new Vue(LoginForm).$mount();
-    expect(instance.username).toBe('');
+  test('ID가 이메일 형식이 아니면 경고 메세지가 출력된다', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test',
+          password: '1234',
+        };
+      },
+    });
+    const warningText = wrapper.find('.warning');
+    //console.log(warningText.html());
+    expect(warningText.exists()).toBeTruthy();
+  });
+
+  test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: '',
+          password: '',
+        };
+      },
+    });
+    const button = wrapper.find('button');
+    // button.element.disabled
+    expect(button.element.disabled).toBeTruthy();
   });
 });
