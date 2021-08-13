e11256fde3517ce11afd3fed93dca45e4285c619
hansoloh <seoyeonoh1022@gmail.com>
2021-08-13 11:57:43 +0900

    0813

---
+++ VueApp/vue-default/src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+//import Contact from '../views/Contact.vue'
+// import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: About
+  // },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/Contact.vue')
+  },
+  {
+    path: '/basic',
+    name: 'Basic',
+    component: () => import(/* webpackChunkName: "about", webpackPrefetch:true */ '../views/Basic.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: () => import(/* webpackChunkName: "about", webpackPrefetch:true */ '../views/DataBinding.vue')
+  },
+  {
+    path: '/databindinghtml',
+    name: 'DataBindingHtml',
+    component: () => import(/* webpackChunkName: "about", webpackPrefetch:true */ '../views/DataBindingHtml.vue')
+  },
+  {
+    path: '/example',
+    name: 'Example',
+    component: () => import(/* webpackChunkName: "about", webpackPrefetch:true */ '../views/Example.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ VueApp/vue-default/src/views/DataBinding.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<h1>{{sampleData}}</h1>
+<h1>{{sampleData2}}</h1>
+</div>
+</template>
+<script>
+export default {
+	name: '',
+	components: {},
+	data() {
+		return {
+			sampleData: 'Hello',
+			sampleData2: 'World'
+		};
+	},
+	setup(){},
+	create(){},
+	mounted(){},
+	unmounted(){},
+	methods:{		
+	}
+}
+</script>
+
+<style>
+
+</style>
