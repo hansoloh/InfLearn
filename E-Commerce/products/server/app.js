7b41066db0bd24738f93141d059312d89e88b693
hansoloh <seoyeonoh1022@gmail.com>
2021-09-08 18:19:43 +0900

    0908

---
+++ E-Commerce/products/server/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({
+	secret: 'secret code',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		secure: true,
+		maxAge: 1000 * 60 * 60, //쿠키 유효시간 1시간
+	}
+}));
+
+const server = app.listen(3000, () => {
+	console.log('Server started at port 3000');
+});
+
+const db = {
+	database: "dev_class",
+	connectionLimit: 10,
+	host: "127.0.0.1",
+	user: "root",
+	password: "mariadb"
+};
+
+const dbPool = require('mysql').createPool(db);
+
+app.post('/api/login',async(request, res) => {	
+
+});
+app.post('/api/logout',async(request, res) => {	
+
+});
+
+const sql = require('./sql.js');
+
+app.post('/api/:alias', async(request, res) => {
+	try{
+		res.send(await req.db(request.params.alias))
+	} catch(error){
+		res.status(500).send({
+			error: err
+		});
+	}
+});
+
+const req = {
+	async db(alias, param = [], where = '') {
+		return new Promise((resolve, reject) => dbPool.query(sql[alias].query + where, param, (error, rows) => {
+		  if (error) {
+			if (error.code != 'ER_DUP_ENTRY')
+			  console.log(error);
+			resolve({
+			  error
+			});
+		  } else resolve(rows);
+		}));
+	  }
+	};
